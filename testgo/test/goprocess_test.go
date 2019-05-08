d0004efc1551f7836ae33a5052ebec3b373770bb
SangKil Park <ht1911@gmail.com>
2019-05-08 15:13:22 +0900

    github.com/jbenet/goprocess

---
+++ testgo/test/goprocess_test.go
@@ -0,0 +1,36 @@
+package goprocess_test
+
+import (
+	"fmt"
+	"github.com/jbenet/goprocess"
+	"time"
+)
+
+func ExampleGo() {
+	p := goprocess.Go(func(p goprocess.Process) {
+		ticker := time.Tick(200 * time.Millisecond)
+		for {
+			select {
+			case <-ticker:
+				fmt.Println("tick")
+			case <-p.Closing():
+				fmt.Println("closing")
+				return
+			}
+		}
+	})
+
+	<-time.After(1100 * time.Millisecond)
+	p.Close()
+	fmt.Println("closed")
+	<-time.After(100 * time.Millisecond)
+
+	// Output:
+	// tick
+	// tick
+	// tick
+	// tick
+	// tick
+	// closing
+	// closed
+}
