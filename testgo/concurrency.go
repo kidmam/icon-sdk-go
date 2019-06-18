2eb48d4a25c7e9223bc3c4af7b61785caccf78d2
SangKil Park <ht1911@gmail.com>
2019-06-19 06:57:05 +0900

    Concurrency Example

---
+++ testgo/concurrency.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	runtime.GOMAXPROCS(1)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	fmt.Println("Starting Go Routines")
+	go func() {
+		defer wg.Done()
+
+		for char := 'a'; char < 'a'+26; char++ {
+			fmt.Printf("%c ", char)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		for number := 1; number < 27; number++ {
+			fmt.Printf("%d ", number)
+		}
+	}()
+
+	fmt.Println("Waiting To Finish")
+	wg.Wait()
+
+	fmt.Println("\nTerminating Program")
+}
